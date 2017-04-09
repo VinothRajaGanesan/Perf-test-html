e5f340a55101c9e18cddad3bbffca112d649756a
Vinoth Raja <vinothraja.ganesan@gmail.com>
2017-04-09 20:08:51 +0530

    perf test

---
+++ test.js
@@ -0,0 +1,9 @@
+(function() {
+   // your page initialization code here
+   // the DOM will be available here
+
+   
+console.log(document.readyState);
+   eval($('[type=esupscript]').html());
+
+})();
